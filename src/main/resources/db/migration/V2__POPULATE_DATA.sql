50e9e7be1bb7af592313f0f7433bf861385ea978
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2021-08-28 13:11:12 +0100

    changed to one line description

--- src/main/resources/db/migration/V2__POPULATE_DATA.sql
+++ src/main/resources/db/migration/V2__POPULATE_DATA.sql
@@ -8,16 +8,7 @@ INSERT INTO day_sessions (date, training_season_id, status) VALUES ('2022-02-18'
 
 -- EXERCISES
 -- INSERT INTO exercises (type, subtype, name, bookpage, beginner, intermediate, advanced, sets, reps, time, description)
-INSERT INTO exercises (type, subtype, name, bookpage, beginner, intermediate, advanced, sets, reps, time, description) VALUES ('Warm up / cool down', 'Full body', 'Simple Sun Salutations', 61, 'TRUE', 'TRUE', 'TRUE', 0, 0, 'T0:02:00', 'Begin standing straight with hands at side. 
-Raise arms out to side and above head, stretching back. 
-With a straight back, reach out and down, touching toes. 
-Straight back and open chest, allowing yourself to rise up a little. 
-Reach back down to toes, place hands on floor, and step back into a plank. 
-Drop into a push up and then push chest upwards into upward dog. 
-Reverse movement back into push up and then push back into downward dog. 
-Step feet forward again, coming into a doubled over position. 
-Raise arms out and up as you straighten your back.  
-Return to starting position, and repeat. ');
+INSERT INTO exercises (type, subtype, name, bookpage, beginner, intermediate, advanced, sets, reps, time, description) VALUES ('Warm up / cool down', 'Full body', 'Simple Sun Salutations', 61, 'TRUE', 'TRUE', 'TRUE', 0, 0, 'T0:02:00', 'Begin standing straight with hands at side. Raise arms out to side and above head, stretching back. With a straight back, reach out and down, touching toes. Straight back and open chest, allowing yourself to rise up a little. Reach back down to toes, place hands on floor, and step back into a plank. Drop into a push up and then push chest upwards into upward dog. Reverse movement back into push up and then push back into downward dog. Step feet forward again, coming into a doubled over position. Raise arms out and up as you straighten your back.  Return to starting position, and repeat.');
 
 -- SESSIONS
 -- INSERT INTO exercises (time, status, name, description, template, day_session_id)
